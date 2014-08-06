5ce80ddfc269f195b85ff0983cae061950a3808f
Nishant967 <rawat.nishant967@gmail.com>
2014-08-06 21:19:46 +0530

    commit

---
+++ Algorithms/Insertionsort/src/Insertsort.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: nishant
+ * Date: 6/8/14
+ * Time: 9:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Insertsort {
+}
